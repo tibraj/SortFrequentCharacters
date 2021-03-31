e67906752187c96492e831d6f48c22b4e974d676
tibraj <tibraj94@gmail.com>
2021-03-31 13:36:16 -0400

    valid palindrome 2 first version

--- app.js
+++ app.js
@@ -2,15 +2,38 @@ function sortFrequency(string) {
     let stack = {};
     for(let char of string) {
         stack[char] ? stack[char]++ : stack[char] = 1;
-    }
+    } //store character frequency
 
     let sortedChars = Object.keys(stack).sort((a, b) => stack[b] - stack[a]);
+    // sort character frequency in descending order
 
     let result = "";
 
     for(let char of sortedChars) {
         result += char.repeat(stack[char]);
-    }
+    } //iterate through array while appending character appearances to result
 
     return result;
+}
+
+function validPalindrome2(string) {
+    let start = 0;
+    let end = string.length - 1;
+
+    while(start < end) {
+        if(string[start] !== string[end]) {
+            return isValid(string.slice(start, end)) || isValid(string.slice(start + 1, end + 1));
+        }
+    }
+
+    return true;
+} 
+
+function isValid(string) {
+    for(let i = 0; i < string.length / 2; i++) {
+        if(string[i] !== string[string.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
