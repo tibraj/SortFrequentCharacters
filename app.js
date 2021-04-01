7746ebca2db6ba8bb2b05cc017847f8a04b104ef
tibraj <tibraj94@gmail.com>
2021-04-01 13:27:10 -0400

    min stack

--- app.js
+++ app.js
@@ -62,3 +62,27 @@ function validPalindrome3(string) {
     }
     return true;
 }
+
+// Min Stack
+function MinStack() {
+    this.elements = [];
+}
+
+MinStack.prototype.push = function(x) {
+    this.elements.push({
+        value: x,
+        min: this.elements.length === 0 ? x : Math.min(x, this.getMin())
+    });
+};
+
+MinStack.prototype.pop = function() {
+    this.elements.pop();
+};
+
+MinStack.prototype.top = function() {
+    return this.elements[this.elements.length - 1].value;
+}
+
+MinStack.prototype.getMin = function() {
+    return this.elements[this.elements.length - 1].min;
+}
